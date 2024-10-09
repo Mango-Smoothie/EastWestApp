6359646f65030178c6c2c632cee86dcd7ba670ce
Nathan Tran <nathan.tran.cs@gmail.com>
2024-10-08 23:29:47 -0400

    added navbar and inventory list

--- EastWestApp/src/app/app.routes.ts
+++ EastWestApp/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InventoryListComponent } from './components/inventory-list/inventory-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/inventory', pathMatch: 'full' },
+  { path: 'inventory', component: InventoryListComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
